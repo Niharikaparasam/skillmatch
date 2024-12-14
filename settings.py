38bf81932020ee1d04e732d76b873e29d11fd3ae
Niharikaparasam <155382142+Niharikaparasam@users.noreply.github.com>
2024-12-14 21:31:18 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recommendations',  # Your custom app
+    'recommendations'# Your custom app
     # Add other apps like 'django.contrib.sites' if you're using them
 ]
 
